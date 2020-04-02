21aafee625699fb755496f54a6d1b5e50b06d475
dedehudianto12 <dedehudianto12@gmail.com>
2020-04-03 00:37:57 +0700

    deploy1

--- server-deploy/controllers/userController.js
+++ server-deploy/controllers/userController.js
@@ -40,7 +40,6 @@ class UserController {
         }
         User.findOne({ where: { email: req.body.email } })
             .then((user) => {
-                console.log(user)
                 if (!user) {
                     next({
                         status: 404,
